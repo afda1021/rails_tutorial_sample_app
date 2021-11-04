9781794d844e9bf83e2baa5b6fd81ab224093477
yota.inoue <yota.inoue@ip-192-168-0-20.ap-northeast-1.compute.internal>
2021-11-04 21:00:20 +0900

    3章：Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about' #static_pages_about_urlが使えるようになる
   get 'static_pages/contact'
-  root 'static_pages#home'
 end
