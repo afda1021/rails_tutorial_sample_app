19ef3fa10e4ed9e7f0e54efe97b12da33acc34d3
afda1021 <63727686+afda1021@users.noreply.github.com>
2023-03-06 17:48:26 +0900

    testでもpryを使えるようにした、演習9.3.1追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password]) # authenticateはパスワードをもとに認証、user情報 or falseを返す
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password]) # authenticateはパスワードをもとに認証、user情報 or falseを返す
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user) #(リスト 9.7, 9.23)
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user) #(リスト 9.7, 9.23)
       # redirect_to user # user_url(user) → users#show  削除(リスト10.32)
-      redirect_back_or user # (リスト10.32)
+      redirect_back_or @user # (リスト10.32)
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
